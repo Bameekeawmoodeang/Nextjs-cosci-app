5eb993e392143d858d4607556a4fde586e37319f
gg <gungging424@gmail.com>
2025-05-27 11:36:07 +0700

    add event click

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 export default function Appwelcome(){
     const title = 'Welcome to cosci';
     const curentYear = <p>2025</p>;
     const isShow = true;
+    const handleClick = () => {
+        alert('Hello Typescript');
+    }
 
 
     return (
     <> 
         <p>{title.toUpperCase()}</p>
+        <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>กดได้เลย!</button>
         {curentYear}
         {
             isShow && <p>Date: 10/10/1998</p>
